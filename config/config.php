f4b494087015b1d988cef7529475483a60411cc6
haile12michael12 <michaelhaile12@yahoo.com>
2024-03-30 07:09:24 -0700

    add

---
+++ config/config.php
@@ -0,0 +1,33 @@
+<?php
+
+    try {
+        //host
+        $host = "localhost";
+
+        //dbname
+        $dbname = "cleanblog";
+
+        //user
+        $user = "root";
+
+        //pass
+        $pass = "";
+
+
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+
+    }
+
+
+    
+
+
+    // if($conn == true) {
+    //     echo "conn works fine";
+    // } else {
+    //     echo "conn err";
+    // }
---
+++ posts/delete.php
@@ -0,0 +1,39 @@
+<?php require "../includes/navbar.php"; ?>
+
+
+<?php require "../config/config.php"; ?>
+
+<?php 
+
+    if(isset($_GET['del_id'])) {
+        $id = $_GET['del_id'];
+
+        $select = $conn->query("SELECT * FROM posts WHERE id =' $id'");
+        $select->execute();
+        $posts = $select->fetch(PDO::FETCH_OBJ);
+
+        if($_SESSION['user_id'] !== $posts->user_id) {
+            header('location: http://localhost/clean-blog/index.php');
+
+        } else {
+            unlink("images/" . $posts->img . "");
+
+
+
+            $delete = $conn->prepare("DELETE FROM posts WHERE id = :id");
+            $delete->execute([
+                ':id' => $id
+            ]);
+        }
+
+       
+
+        header('location: http://localhost/clean-blog/index.php');
+
+        
+    }  else {
+        header("location: http://localhost/clean-blog/404.php");
+       
+    }  
+
+?>
